5e20e17fad6b1c6fba8dc114c2bd48f5a4684c34
zayaankhan <zayaan.khan@mail.com>
2017-08-17 10:43:11 +0530

    Update AddressbookUI.java

--- src/main/java/com/edurekademo/tutorial/addressbook/AddressbookUI.java
+++ src/main/java/com/edurekademo/tutorial/addressbook/AddressbookUI.java
@@ -71,7 +71,7 @@ public class AddressbookUI extends UI {
          */
         newContact.addClickListener(e -> contactForm.edit(new Contact()));
 
-        filter.setInputPrompt("Enter the name you want to search for -- Example aamir");
+        filter.setInputPrompt("I changed this after adding a remove build to check if its getting propagated across the pipeline.");
         filter.addTextChangeListener(e -> refreshContacts(e.getText()));
 
         contactList
